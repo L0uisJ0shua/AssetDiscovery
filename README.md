6c85f813d9ecd29a0c0accde89fdbcf3f3fb6738
L0uisJ0shua <sun-louis@hotmail.com>
2021-06-23 14:07:09 +0800

    Updated README.md and setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
- # AssetDiscover
+# AssetDiscovery
+
+AssetDiscover is tool scripted for the use of discovery of assets from a given domain or a list of given domain and run vulnerability scans on web domains for low-hanging fruits and known CVEs. This tool is a combination of amaas, httprobe, nuclei and many other domain enumeration tools. 
+
+# Setup
+To setup, there are a few prerequisite tools to install:
+    - Go
+    - Nuclei
+    - amass
+    - subfinder
+    - crobat
+    - httprobe
+
+There is currently a work-in-progress script that we are working on to help standardise installation to make it easier to setup.
+
+# Usage
+
+Basic Usage of the tool:
+
+```
+python3 AssetDiscover.py -d <domain> --nuclei -t <directory of nuclei templates>
+```
+
+Usage of tool with bruteforce subdomain
+```
+python3 AssetDiscover.py -d <domain> --brute-force -w <wordlist> --nuclei -t <directory of nuclei templates>
+```
+
+## Why use this tool?
+This tool is created to help combine functionalities for different tools to better identify assets belonging to target and identify low-hanging fruits or common CVE vulnerabilities found in websites. This will allow users of the tool to identify targets and enumerate general web technology used by the website.
--- setup.sh
+++ setup.sh
@@ -1,30 +1,57 @@
 #!/usr/bin/bash
 
 ## TODO ##
-# Script is roughly tested but does not work properly to install all the
-# components properly yet
+# Require testing if the script works well
+
+set -e
 
 # Go
-echo "[+] Grabbing Go..."
-wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
-echo "[+] Installing Go lang..."
-sudo rm /usr/local/go
-sudo tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+echo "[-] Checking if Go is present..."
+if ! go_loc="$(type -p "go --version")" || [[ -z $go_loc ]]; then
+    echo "[+] Grabbing Go..."
+    wget https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+    echo "[+] Installing Go lang..."
+    sudo rm /usr/local/go
+    sudo tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+    export PATH="$PATH:/usr/local/go"
+    export PATH="$PATH:$HOME/go/bin"
+    go --version
+    echo "[+] Go is installed successfully."
+else
+    echo "[+] Go already installed."
+fi
 
-# httpx
-echo "[+] Installing httpx..."
-GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
-
-# aquatone
-echo "[+] Grabbing aquatone..."
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
-echo "[+] Installing aquatone by moving it to /usr/bin..."
-sudo tar -C /usr/bin -xzf aquatone_linux_amd64_1.7.0.zip
+# httprobe
+echo "[-] Checking if httprobe is installed..."
+if ! probe_loc="$(type -p "httprobe")" || [[ -z $probe_loc ]]; then
+    echo "[+] Installing httpprobe..."
+    go get -u github.com/tomnomnom/httprobe
+    echo "[+] httprobe is installed successfully."
+else
+    echo "[+] httprobe already installed."
+fi
 
 # amass
-echo "[+] Installing OWASP Amass..."
-sudo apt install amass
+echo "[-] Checking if OWASP Amass is installed..."
+if ! amass_loc="$(type -p "amass -version")" || [[ -z $amass_loc ]]; then
+    echo "[+] Installing OWASP Amass..."
+    sudo apt install amass
+    amass -version
+    echo "[+] OWASP Amass is installed successfully"
+else
+    echo "[+] OWASP Amass already installed."
+fi
+
 
 # nuclei
-echo "[+] Installing Nuclei..."
-GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+echo "[-] Checking if Nuclei is installed..."
+if ! amass_loc="$(type -p "nuclei -version")" || [[ -z $amass_loc ]]; then
+    echo "[+] Installing Nuclei..."
+    GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    nuclei -version
+    echo "[+] Nuclei is installed successfully"
+else
+    echo "[+] Nuclei already installed."
+fi
+
+echo "[+] Setup of pre-requsite for AssetDiscovery is successfully complated."
